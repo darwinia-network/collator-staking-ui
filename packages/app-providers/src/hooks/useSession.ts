38c1db46181d28dca2682407ef6fd93b57c6ac6f
Ismail Mbarack (Nas) <isunaslabs@users.noreply.github.com>
2023-01-16 14:59:24 +0800

    UI bug fix

--- packages/app-providers/src/hooks/useSession.ts
+++ packages/app-providers/src/hooks/useSession.ts
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 const useSession = () => {
   const [sessionDuration, setSessionDuration] = useState<number>(24 * 60 * 60);
-  const [unbondingDuration, setUnbondingDuration] = useState<number>(10 * 24 * 60 * 60);
+  const [unbondingDuration, setUnbondingDuration] = useState<number>(14 * 24 * 60 * 60);
   return {
     sessionDuration,
     unbondingDuration,
