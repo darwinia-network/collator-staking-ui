0623f2de8457e7ad495929fce08973239d6a0d00
fisher <hi@denny.im>
2023-03-10 22:58:25 +0800

    fix quote marks

--- packages/app/src/components/JoinCollatorModal/index.tsx
+++ packages/app/src/components/JoinCollatorModal/index.tsx
@@ -138,7 +138,7 @@ const JoinCollatorModal = forwardRef<JoinCollatorRefs>((props, ref) => {
             className={"text-halfWhite text-12 divider border-b pb-[10px]"}
             dangerouslySetInnerHTML={{
               __html: t(localeKeys.howToJoinCollator, {
-                runNodeUrl: "https://docs.darwinia.network/how-to-become-a-collator-679e363b68ab47189bde7826c8258c1d,
+                runNodeUrl: "https://docs.darwinia.network/how-to-become-a-collator-679e363b68ab47189bde7826c8258c1d",
                 tutorialUrl: "https://docs.darwinia.network/how-to-become-a-collator-679e363b68ab47189bde7826c8258c1d",
               }),
             }}
