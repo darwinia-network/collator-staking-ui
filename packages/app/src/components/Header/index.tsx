e9e08591ab0290239a6abc419cdc77f1df68276c
JayJay1024 <lygm1062@gmail.com>
2023-06-29 15:38:00 +0800

    rename file

--- packages/app/src/components/Header/index.tsx
+++ packages/app/src/components/Header/index.tsx
@@ -10,7 +10,7 @@ import JazzIcon from "../JazzIcon";
 import { utils } from "ethers";
 import { JoinCollatorModal, JoinCollatorRefs } from "../JoinCollatorModal";
 import { ManageCollatorModal, ManageCollatorRefs } from "../ManageCollatorModal";
-import { CustomRpc } from "../AddCustomRpc";
+import { CustomRpc } from "../CustomRpc";
 
 export const Header = () => {
   const [networkOptionsTrigger, setNetworkOptionsTrigger] = useState<HTMLDivElement | null>(null);
