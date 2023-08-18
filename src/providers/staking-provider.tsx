ae0dc594e07a58b733150477094fa9f7cae82388
JayJay1024 <lygm1062@gmail.com>
2023-08-18 15:55:35 +0800

    improve code

--- src/providers/staking-provider.tsx
+++ src/providers/staking-provider.tsx
@@ -1,14 +1,20 @@
 "use client";
 
-import { useApi, useApp, useBlock } from "@/hooks";
-import { calcKtonReward, calcMonths, getChainConfig, stakingToPower } from "@/utils";
+import {
+  useActiveCollators,
+  useApi,
+  useCollatorCommission,
+  useCollatorLastSessionBlocks,
+  useCollatorNominators,
+  useCollatorPower,
+  useDeposits,
+  useLedger,
+  useNominatorCollators,
+  usePool,
+} from "@/hooks";
+import { stakingToPower } from "@/utils";
 import { PropsWithChildren, createContext, useCallback, useEffect, useMemo, useState } from "react";
-import type { Balance } from "@polkadot/types/interfaces";
-import type { Option, Vec, StorageKey } from "@polkadot/types";
-import type { AnyTuple, Codec } from "@polkadot/types/types";
-import { useAccount } from "wagmi";
-import { Subscription, EMPTY, from } from "rxjs";
-import type { DarwiniaStakingLedger, Deposit, DepositCodec, UnbondingInfo } from "@/types";
+import type { Deposit, UnbondingInfo } from "@/types";
 
 interface StakingCtx {
   deposits: Deposit[];
@@ -20,16 +26,10 @@ interface StakingCtx {
   stakedKton: bigint;
   stakedDeposit: bigint;
   activeCollators: string[];
+  collatorPower: { [collator: string]: bigint | undefined };
   collatorCommission: { [collator: string]: string | undefined };
   collatorLastSessionBlocks: { [collator: string]: number | undefined };
-  collatorNominators: {
-    [collator: string]:
-      | {
-          totalStakedPower: bigint;
-          nominators: { address: string; stakedPower: bigint }[];
-        }
-      | undefined;
-  };
+  collatorNominators: { [collator: string]: string[] | undefined };
   nominatorCollators: { [nominator: string]: string[] | undefined };
   unbondingRing: Omit<UnbondingInfo, "depositId">[];
   unbondingKton: Omit<UnbondingInfo, "depositId">[];
@@ -41,6 +41,7 @@ interface StakingCtx {
   isRingPoolInitialized: boolean;
   isKtonPoolInitialized: boolean;
   isActiveCollatorsInitialized: boolean;
+  isCollatorPowerInitialized: boolean;
   isCollatorCommissionInitialized: boolean;
   isCollatorLastSessionBlocksInitialized: boolean;
   isCollatorNominatorsInitialized: boolean;
@@ -64,6 +65,7 @@ const defaultValue: StakingCtx = {
   stakedKton: 0n,
   stakedDeposit: 0n,
   activeCollators: [],
+  collatorPower: {},
   collatorCommission: {},
   collatorLastSessionBlocks: {},
   collatorNominators: {},
@@ -78,6 +80,7 @@ const defaultValue: StakingCtx = {
   isRingPoolInitialized: false,
   isKtonPoolInitialized: false,
   isActiveCollatorsInitialized: false,
+  isCollatorPowerInitialized: false,
   isCollatorCommissionInitialized: false,
   isCollatorLastSessionBlocksInitialized: false,
   isCollatorNominatorsInitialized: false,
@@ -94,54 +97,35 @@ const defaultValue: StakingCtx = {
 export const StakingContext = createContext(defaultValue);
 
 export function StakingProvider({ children }: PropsWithChildren<unknown>) {
-  const [deposits, setDeposits] = useState(defaultValue.deposits);
-  const [stakedDeposits, setStakedDeposits] = useState(defaultValue.stakedDeposits);
-  const [ringPool, setRingPool] = useState(defaultValue.ringPool);
-  const [ktonPool, setKtonPool] = useState(defaultValue.ktonPool);
-  const [stakedRing, setStakedRing] = useState(defaultValue.stakedRing);
-  const [stakedKton, setStakedKton] = useState(defaultValue.stakedKton);
-  const [stakedDeposit, setStakedDeposit] = useState(defaultValue.stakedDeposit);
-  const [activeCollators, setActiveCollators] = useState(defaultValue.activeCollators);
-  const [collatorCommission, setCollatorCommission] = useState(defaultValue.collatorCommission);
-  const [collatorLastSessionBlocks, setCollatorLastSessionBlocks] = useState(defaultValue.collatorLastSessionBlocks);
-  const [collatorNominators, setCollatorNominators] = useState(defaultValue.collatorNominators);
-  const [nominatorCollators, setNominatorCollators] = useState(defaultValue.nominatorCollators);
-  const [unbondingRing, setUnbondingRing] = useState(defaultValue.unbondingRing);
-  const [unbondingKton, setUnbondingKton] = useState(defaultValue.unbondingKton);
-  const [unbondingDeposits, setUnbondingDeposits] = useState(defaultValue.unbondingDeposits);
+  const { polkadotApi } = useApi();
   const [minimumDeposit, setMinimumDeposit] = useState(defaultValue.minimumDeposit);
 
-  const [isLedgersInitialized, setIsLedgersInitialized] = useState(defaultValue.isLedgersInitialized);
-  const [isDepositsInitialized, setIsDepositsInitialized] = useState(defaultValue.isDepositsInitialized);
-  const [isRingPoolInitialized, setIsRingPoolInitialized] = useState(defaultValue.isRingPoolInitialized);
-  const [isKtonPoolInitialized, setIsKtonPoolInitialized] = useState(defaultValue.isKtonPoolInitialized);
-  const [isActiveCollatorsInitialized, setIsActiveCollatorsInitialized] = useState(
-    defaultValue.isActiveCollatorsInitialized
+  const { collatorLastSessionBlocks, isCollatorLastSessionBlocksInitialized } =
+    useCollatorLastSessionBlocks(defaultValue);
+  const { ringPool, ktonPool, isRingPoolInitialized, isKtonPoolInitialized } = usePool(defaultValue);
+  const { activeCollators, isActiveCollatorsInitialized } = useActiveCollators(defaultValue);
+  const { deposits, isDepositsInitialized } = useDeposits(defaultValue);
+  const {
+    stakedRing,
+    stakedKton,
+    stakedDeposits,
+    stakedDeposit,
+    unbondingRing,
+    unbondingKton,
+    unbondingDeposits,
+    isLedgersInitialized,
+  } = useLedger(deposits, defaultValue);
+  const { collatorCommission, isCollatorCommissionInitialized, isCollatorCommissionLoading, updateCollatorCommission } =
+    useCollatorCommission(defaultValue);
+  const { nominatorCollators, isNominatorCollatorsInitialized, isNominatorCollatorsLoading, updateNominatorCollators } =
+    useNominatorCollators(defaultValue);
+  const { collatorNominators, isCollatorNominatorsInitialized } = useCollatorNominators(defaultValue);
+  const { collatorPower, isCollatorPowerInitialized } = useCollatorPower(
+    collatorNominators,
+    ringPool,
+    ktonPool,
+    defaultValue
   );
-  const [isCollatorCommissionInitialized, setIsCollatorCommissionInitialized] = useState(
-    defaultValue.isCollatorCommissionInitialized
-  );
-  const [isCollatorLastSessionBlocksInitialized, setIsCollatorLastSessionBlocksInitialized] = useState(
-    defaultValue.isCollatorLastSessionBlocksInitialized
-  );
-  const [isCollatorNominatorsInitialized, setIsCollatorNominatorsInitialized] = useState(
-    defaultValue.isCollatorNominatorsInitialized
-  );
-  const [isNominatorCollatorsInitialized, setIsNominatorCollatorsInitialized] = useState(
-    defaultValue.isNominatorCollatorsInitialized
-  );
-
-  const [isNominatorCollatorsLoading, setIsNominatorCollatorsLoading] = useState(
-    defaultValue.isNominatorCollatorsLoading
-  );
-  const [isCollatorCommissionLoading, setIsCollatorCommissionLoading] = useState(
-    defaultValue.isNominatorCollatorsLoading
-  );
-
-  const { address } = useAccount();
-  const { polkadotApi } = useApi();
-  const { blockTimestamp, blockNumber } = useBlock();
-  const { activeChain } = useApp();
 
   const power = useMemo(
     () => stakingToPower(stakedRing + stakedDeposit, stakedKton, ringPool, ktonPool),
@@ -155,314 +139,9 @@ export function StakingProvider({ children }: PropsWithChildren<unknown>) {
     [ringPool, ktonPool]
   );
 
-  const updateNominatorCollators = useCallback(() => {
-    if (polkadotApi) {
-      setIsNominatorCollatorsLoading(true);
-
-      return from(polkadotApi.query.darwiniaStaking.nominators.entries()).subscribe({
-        next: (entries) =>
-          setNominatorCollators(
-            (entries as [StorageKey<AnyTuple>, Option<Codec>][]).reduce((acc, cur) => {
-              const [key, result] = cur;
-              const nominator = key.args[0].toHuman() as string;
-              if (result.isSome) {
-                const collator = result.unwrap().toHuman() as string;
-                return { ...acc, [nominator]: [collator] };
-              }
-              return acc;
-            }, {})
-          ),
-        error: console.error,
-        complete: () => {
-          setIsNominatorCollatorsInitialized(true);
-          setIsNominatorCollatorsLoading(false);
-        },
-      });
-    } else {
-      setNominatorCollators({});
-      return EMPTY.subscribe();
-    }
-  }, [polkadotApi]);
-
-  const updateCollatorCommission = useCallback(() => {
-    if (polkadotApi) {
-      setIsCollatorCommissionLoading(true);
-
-      return from(polkadotApi.query.darwiniaStaking.collators.entries()).subscribe({
-        next: (entries) =>
-          setCollatorCommission(
-            entries.reduce((acc, cur) => {
-              const [key, result] = cur;
-              const collator = key.args[0].toHuman() as string;
-              const commission = `${result.toHuman()}`;
-              return { ...acc, [collator]: commission };
-            }, {})
-          ),
-        error: console.error,
-        complete: () => {
-          setIsCollatorCommissionInitialized(true);
-          setIsCollatorCommissionLoading(false);
-        },
-      });
-    } else {
-      setCollatorCommission({});
-      return EMPTY.subscribe();
-    }
-  }, [polkadotApi]);
-
   useEffect(() => {
     setMinimumDeposit(BigInt(polkadotApi?.consts.deposit.minLockingAmount.toString() || 0));
   }, [polkadotApi]);
-
-  // active collators
-  useEffect(() => {
-    let unsub = () => undefined;
-
-    polkadotApi?.query.session
-      .validators((addresses: Codec) => {
-        // these are the collators that are active in this session
-        setActiveCollators(addresses.toJSON() as string[]);
-      })
-      .then((_unsub) => {
-        unsub = _unsub as unknown as typeof unsub;
-      })
-      .catch(console.error)
-      .finally(() => setIsActiveCollatorsInitialized(true));
-
-    return () => unsub();
-  }, [polkadotApi]);
-
-  // collator's nominators
-  useEffect(() => {
-    let unsub = () => undefined;
-
-    polkadotApi?.query.darwiniaStaking.exposures
-      .entries((entries: [StorageKey<AnyTuple>, Codec][]) => {
-        setCollatorNominators(
-          entries.reduce((acc, cur) => {
-            const [key, result] = cur;
-            const collator = key.args[0].toHuman() as string;
-            const exposure = result.toJSON() as {
-              total: number | string;
-              nominators: { value: number | string; who: string }[];
-            };
-            return {
-              ...acc,
-              [collator]: {
-                totalStakedPower: BigInt(exposure.total),
-                nominators: exposure.nominators.map(({ value, who }) => ({ address: who, stakedPower: BigInt(value) })),
-              },
-            };
-          }, {})
-        );
-      })
-      .then((_unsub) => {
-        unsub = _unsub as unknown as typeof unsub;
-      })
-      .catch(console.error)
-      .finally(() => setIsCollatorNominatorsInitialized(true));
-
-    return () => unsub();
-  }, [polkadotApi]);
-
-  // collator last session blocks
-  useEffect(() => {
-    let unsub = () => undefined;
-
-    polkadotApi?.query.darwiniaStaking
-      .rewardPoints((points: Codec) => {
-        const [_, collatorPoints] = points.toJSON() as [number, { [address: string]: number }]; // [totalPoint, { collator: collatorPoint }]
-        const staticNumber = 20; // this staticNumber = 20 was given by the backend
-
-        setCollatorLastSessionBlocks(
-          Object.keys(collatorPoints).reduce((acc, cur) => {
-            const collatorPoint = collatorPoints[cur];
-            const blocks = collatorPoint / staticNumber;
-            return { ...acc, [cur]: blocks };
-          }, {})
-        );
-      })
-      .then((_unsub) => {
-        unsub = _unsub as unknown as typeof unsub;
-      })
-      .catch(console.error)
-      .finally(() => setIsCollatorLastSessionBlocksInitialized(true));
-
-    return () => unsub();
-  }, [polkadotApi]);
-
-  // ring pool
-  useEffect(() => {
-    let unsub = () => undefined;
-
-    polkadotApi?.query.darwiniaStaking
-      .ringPool((value: Balance) => setRingPool(value.toBigInt()))
-      .then((_unsub) => {
-        unsub = _unsub as unknown as typeof unsub;
-      })
-      .catch(console.error)
-      .finally(() => setIsRingPoolInitialized(true));
-
-    return () => unsub();
-  }, [polkadotApi]);
-
-  // kton pool
-  useEffect(() => {
-    let unsub = () => undefined;
-
-    polkadotApi?.query.darwiniaStaking
-      .ktonPool((value: Balance) => setKtonPool(value.toBigInt()))
-      .then((_unsub) => {
-        unsub = _unsub as unknown as typeof unsub;
-      })
-      .catch(console.error)
-      .finally(() => setIsKtonPoolInitialized(true));
-
-    return () => unsub();
-  }, [polkadotApi]);
-
-  // deposits (every block)
-  useEffect(() => {
-    let sub$$: Subscription | undefined;
-
-    if (address && polkadotApi && blockTimestamp) {
-      sub$$ = from(polkadotApi.query.deposit.deposits(address) as Promise<Option<Vec<DepositCodec>>>).subscribe({
-        next: (depositsOpt) => {
-          if (depositsOpt.isSome) {
-            setDeposits(
-              depositsOpt.unwrap().map((item) => {
-                const startTime = item.startTime.toNumber();
-                const expiredTime = item.expiredTime.toNumber();
-                const depositRing = item.value.toBigInt();
-
-                return {
-                  id: item.id.toNumber(),
-                  startTime,
-                  accountId: address,
-                  reward: calcKtonReward(depositRing, calcMonths(startTime, expiredTime)),
-                  expiredTime,
-                  value: depositRing,
-                  canEarlyWithdraw: blockTimestamp < expiredTime,
-                  inUse: item.inUse.isTrue,
-                };
-              })
-            );
-          } else {
-            setDeposits([]);
-          }
-        },
-        error: console.error,
-        complete: () => setIsDepositsInitialized(true),
-      });
-    } else {
-      setDeposits([]);
-    }
-
-    return () => sub$$?.unsubscribe();
-  }, [address, polkadotApi, blockTimestamp]);
-
-  // ledgers
-  useEffect(() => {
-    let sub$$: Subscription | undefined;
-
-    if (address && polkadotApi) {
-      const { secondsPerBlock } = getChainConfig(activeChain);
-
-      sub$$ = from(
-        polkadotApi.query.darwiniaStaking.ledgers(address) as Promise<Option<DarwiniaStakingLedger>>
-      ).subscribe({
-        next: (ledgerOpt) => {
-          if (ledgerOpt.isSome) {
-            const ledgerData = ledgerOpt.unwrap().toJSON() as unknown as DarwiniaStakingLedger;
-            const now = Date.now();
-
-            const _unbondingDeposits = (ledgerData.unstakingDeposits || []).map(([depositId, lastBlockNumber]) => {
-              const blocksLeft = lastBlockNumber - blockNumber;
-              const secondsLeft = blocksLeft * secondsPerBlock;
-
-              return {
-                depositId,
-                amount: deposits.find(({ id }) => id === depositId)?.value || 0n,
-                expiredAtBlock: lastBlockNumber,
-                isExpired: blockNumber >= lastBlockNumber,
-                expiredTimestamp: now + secondsLeft * 1000,
-              };
-            });
-
-            const _unbondingRing = (ledgerData.unstakingRing || []).map(([amount, lastBlockNumber]) => {
-              const blocksLeft = lastBlockNumber - blockNumber;
-              const secondsLeft = blocksLeft * secondsPerBlock;
-
-              return {
-                amount: BigInt(amount),
-                expiredAtBlock: lastBlockNumber,
-                isExpired: blockNumber >= lastBlockNumber,
-                expiredTimestamp: now + secondsLeft * 1000,
-              };
-            });
-
-            const _unbondingKton = (ledgerData.unstakingKton || []).map(([amount, lastBlockNumber]) => {
-              const blocksLeft = lastBlockNumber - blockNumber;
-              const secondsLeft = blocksLeft * secondsPerBlock;
-
-              return {
-                amount: BigInt(amount),
-                expiredAtBlock: lastBlockNumber,
-                isExpired: blockNumber >= lastBlockNumber,
-                expiredTimestamp: now + secondsLeft * 1000,
-              };
-            });
-
-            setStakedDeposit(
-              deposits
-                .filter(({ id }) => ledgerData.stakedDeposits?.includes(id))
-                .reduce((acc, cur) => acc + cur.value, 0n)
-            );
-            setStakedDeposits(ledgerData.stakedDeposits || []);
-
-            setStakedRing(BigInt(ledgerData.stakedRing));
-            setStakedKton(BigInt(ledgerData.stakedKton));
-
-            setUnbondingRing(_unbondingRing);
-            setUnbondingKton(_unbondingKton);
-            setUnbondingDeposits(_unbondingDeposits);
-          } else {
-            setStakedDeposit(0n);
-            setStakedDeposits([]);
-            setStakedRing(0n);
-            setStakedKton(0n);
-            setUnbondingRing([]);
-            setUnbondingKton([]);
-            setUnbondingDeposits([]);
-          }
-        },
-        error: console.error,
-        complete: () => setIsLedgersInitialized(true),
-      });
-    } else {
-      setStakedDeposit(0n);
-      setStakedDeposits([]);
-      setStakedRing(0n);
-      setStakedKton(0n);
-      setUnbondingRing([]);
-      setUnbondingKton([]);
-      setUnbondingDeposits([]);
-    }
-
-    return () => sub$$?.unsubscribe();
-  }, [address, deposits, polkadotApi, activeChain, blockNumber]);
-
-  // nominator's collator
-  useEffect(() => {
-    const sub$$ = updateNominatorCollators();
-    return () => sub$$.unsubscribe();
-  }, [updateNominatorCollators]);
-
-  // collator commission
-  useEffect(() => {
-    const sub$$ = updateCollatorCommission();
-    return () => sub$$.unsubscribe();
-  }, [updateCollatorCommission]);
 
   return (
     <StakingContext.Provider
@@ -476,6 +155,7 @@ export function StakingProvider({ children }: PropsWithChildren<unknown>) {
         stakedKton,
         stakedDeposit,
         activeCollators,
+        collatorPower,
         collatorCommission,
         collatorLastSessionBlocks,
         collatorNominators,
@@ -490,6 +170,7 @@ export function StakingProvider({ children }: PropsWithChildren<unknown>) {
         isRingPoolInitialized,
         isKtonPoolInitialized,
         isActiveCollatorsInitialized,
+        isCollatorPowerInitialized,
         isCollatorCommissionInitialized,
         isCollatorLastSessionBlocksInitialized,
         isCollatorNominatorsInitialized,
