fa5bd707df2798748181026b3a0d0502340c2fbe
JayJay1024 <lygm1062@gmail.com>
2024-03-14 15:12:50 +0800

    Parse chains outside rainbow provider

--- src/providers/rainbow-provider.tsx
+++ src/providers/rainbow-provider.tsx
@@ -14,8 +14,7 @@ import { createConfig, WagmiProvider } from "wagmi";
 
 import { APP_NAME_CONF } from "@/config";
 import { getChainConfigs } from "@/utils";
-import { PropsWithChildren, useMemo } from "react";
-import { useApp } from "@/hooks";
+import { PropsWithChildren } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Chain, createClient, http } from "viem";
 
@@ -31,38 +30,34 @@ const connectors = connectorsForWallets(
   { appName: APP_NAME_CONF, projectId }
 );
 
+const chains = getChainConfigs().map(({ chainId, name, nativeToken, explorer, rpcMetas }) => ({
+  id: chainId,
+  name,
+  network: name.toLowerCase().split(" ").join("-"),
+  nativeCurrency: {
+    name: nativeToken.symbol,
+    symbol: nativeToken.symbol,
+    decimals: nativeToken.decimals,
+  },
+  rpcUrls: {
+    default: {
+      http: rpcMetas.filter(({ url }) => url.startsWith("http")).map(({ url }) => url),
+      webSocket: rpcMetas.filter(({ url }) => url.startsWith("ws")).map(({ url }) => url),
+    },
+    public: {
+      http: rpcMetas.filter(({ url }) => url.startsWith("http")).map(({ url }) => url),
+      webSocket: rpcMetas.filter(({ url }) => url.startsWith("ws")).map(({ url }) => url),
+    },
+  },
+  blockExplorers: {
+    default: {
+      url: explorer.url,
+      name: explorer.name,
+    },
+  },
+})) as unknown as [Chain, ...Chain[]];
+
 export function RainbowProvider({ children }: PropsWithChildren<unknown>) {
-  const { activeChain, activeRpc } = useApp();
-
-  const chains = useMemo(() => {
-    return getChainConfigs().map(({ chainId, name, nativeToken, explorer }) => ({
-      id: chainId,
-      name,
-      network: name.toLowerCase().split(" ").join("-"),
-      nativeCurrency: {
-        name: nativeToken.symbol,
-        symbol: nativeToken.symbol,
-        decimals: nativeToken.decimals,
-      },
-      rpcUrls: {
-        default: {
-          http: activeRpc.url.startsWith("http") ? [activeRpc.url] : [],
-          webSocket: activeRpc.url.startsWith("ws") ? [activeRpc.url] : [],
-        },
-        public: {
-          http: activeRpc.url.startsWith("http") ? [activeRpc.url] : [],
-          webSocket: activeRpc.url.startsWith("ws") ? [activeRpc.url] : [],
-        },
-      },
-      blockExplorers: {
-        default: {
-          url: explorer.url,
-          name: explorer.name,
-        },
-      },
-    })) as unknown as [Chain, ...Chain[]];
-  }, [activeRpc.url]);
-
   return (
     <WagmiProvider
       config={createConfig({
@@ -75,7 +70,6 @@ export function RainbowProvider({ children }: PropsWithChildren<unknown>) {
         <RainbowKitProvider
           theme={darkTheme({ borderRadius: "none", accentColor: "#FF0083" })}
           appInfo={{ appName: APP_NAME_CONF }}
-          initialChain={activeChain}
         >
           {children}
         </RainbowKitProvider>
